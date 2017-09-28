1ac9c7b81034f7eddc5b9c4151e83dd072e35c0c
Wladimir J. van der Laan <laanwj@gmail.com>
2017-09-28 13:46:00 +0200

    Merge #10858: [RPC] Add "errors" field to getblockchaininfo and unify "errors" field in get*info RPCs
    395cef7 Change getmininginfo errors field to warnings (Andrew Chow)
    8502b20 Unify help text for GetWarnings output in get*info RPCs (Andrew Chow)
    f77f0e4 Add warnings field to getblockchaininfo (Andrew Chow)
    
    Pull request description:
    
      The `getblockchaininfo` output does not contain the `errors` field which the `getinfo`, `getmininginfo`, and `getnetworkinfo` RPCs have. It should have it as the errors pertain to the blockchain. This PR adds that field.
    
      This PR also unifies the help text for the `errors` field and its output position so that all of the `get*info` commands are consistent.
    
      `getnetworkinfo`'s `errors` field is named `warnings`. I did not change this even though it is inconsistent since this naming has been in use for a long time.
    
    Tree-SHA512: 385ab6acfee67fc8816f4d51ab2bd7a623264c7973906dfbab0a171f199e9db16fde19093a5bc3dfbdd4ff5f19d2186b646eb6b3bae0a4d7c9add43650a4a9d9

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -28,6 +28,7 @@
 #include "util.h"
 #include "utilstrencodings.h"
 #include "hash.h"
+#include "warnings.h"
 
 #include "evo/specialtx.h"
 #include "evo/cbtx.h"
@@ -1430,6 +1431,7 @@ UniValue getblockchaininfo(const JSONRPCRequest& request)
             "        }\n"
             "     }\n"
             "  }\n"
+            "  \"warnings\" : \"...\",         (string) any network and blockchain warnings.\n"
             "}\n"
             "\nExamples:\n"
             + HelpExampleCli("getblockchaininfo", "")
@@ -1476,6 +1478,7 @@ UniValue getblockchaininfo(const JSONRPCRequest& request)
 
         obj.push_back(Pair("pruneheight",        block->nHeight));
     }
+    obj.push_back(Pair("warnings", GetWarnings("statusbar")));
     return obj;
 }
 
