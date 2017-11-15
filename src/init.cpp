1459aa5a7210400bb68ae8e58750709bca70c596
Wladimir J. van der Laan <laanwj@gmail.com>
2017-11-15 14:14:20 +0100

    Merge #10286: Call wallet notify callbacks in scheduler thread (without cs_main)
    89f0312 Remove redundant pwallet nullptr check (Matt Corallo)
    c4784b5 Add a dev notes document describing the new wallet RPC blocking (Matt Corallo)
    3ea8b75 Give ZMQ consistent order with UpdatedBlockTip on scheduler thread (Matt Corallo)
    cb06edf Fix wallet RPC race by waiting for callbacks in sendrawtransaction (Matt Corallo)
    e545ded Also call other wallet notify callbacks in scheduler thread (Matt Corallo)
    17220d6 Use callbacks to cache whether wallet transactions are in mempool (Matt Corallo)
    5d67a78 Add calls to CWallet::BlockUntilSyncedToCurrentChain() in RPCs (Matt Corallo)
    5ee3172 Add CWallet::BlockUntilSyncedToCurrentChain() (Matt Corallo)
    0b2f42d Add CallFunctionInQueue to wait on validation interface queue drain (Matt Corallo)
    2b4b345 Add ability to assert a lock is not held in DEBUG_LOCKORDER (Matt Corallo)
    0343676 Call TransactionRemovedFromMempool in the CScheduler thread (Matt Corallo)
    a7d3936 Add a CValidationInterface::TransactionRemovedFromMempool (Matt Corallo)
    
    Pull request description:
    
      Based on #10179, this effectively reverts #9583, regaining most of the original speedups of #7946.
    
      This concludes the work of #9725, #10178, and #10179.
    
      See individual commit messages for more information.
    
    Tree-SHA512: eead4809b0a75d1fb33b0765174ff52c972e45040635e38cf3686cef310859c1e6b3c00e7186cbd17374c6ae547bfbd6c1718fe36f26c76ba8a8b052d6ed7bc9

--- src/init.cpp
+++ src/init.cpp
@@ -349,6 +349,7 @@ void PrepareShutdown()
 #endif
     UnregisterAllValidationInterfaces();
     GetMainSignals().UnregisterBackgroundSignalScheduler();
+    GetMainSignals().UnregisterWithMempoolSignals(mempool);
 }
 
 /**
@@ -1585,6 +1586,7 @@ bool AppInitMain()
     threadGroup.create_thread(boost::bind(&TraceThread<CScheduler::Function>, "scheduler", serviceLoop));
 
     GetMainSignals().RegisterBackgroundSignalScheduler(scheduler);
+    GetMainSignals().RegisterWithMempoolSignals(mempool);
 
     /* Start the RPC server already.  It will be started in "warmup" mode
      * and not really process calls already (but it will signify connections
