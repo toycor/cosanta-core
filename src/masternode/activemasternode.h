2eb6eb347a50e3d5912262547b447e97ddd98b8b
Alexander Block <ablock84@gmail.com>
2020-03-20 17:11:54 +0100

    Fix issues introduced with asynchronous signal handling (#3369)
    * Introduce SynchronousUpdatedBlockTip signal
    
    This version of UpdatedBlockTip mirrors the asynchronous behavior that we
    had before the introduction of asynchronous signal handling.
    
    * Fix tab spacing in validationinterface.cpp
    
    * Invoke CDeterministicMNManager::UpdatedBlockTip from validation thread
    
    It must be invoked synchronously as otherwise things become inconsistent.
    
    * Call CActiveMasternodeManager::Init with block index
    
    pindexNew in UpdatedBlockTip is not necessarily the current tip, so we
    shouldn't rely on it in Init(). This is due to the async nature of the
    UpdatedBlockTip invocation.

--- src/masternode/activemasternode.h
+++ src/masternode/activemasternode.h
@@ -55,7 +55,7 @@ private:
 public:
     virtual void UpdatedBlockTip(const CBlockIndex* pindexNew, const CBlockIndex* pindexFork, bool fInitialDownload);
 
-    void Init();
+    void Init(const CBlockIndex* pindex);
 
     std::string GetStateString() const;
     std::string GetStatus() const;
