b85b9093f8ecce7f1925d1b8153560e9e4984233
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-05-12 01:33:21 +0200

    qt: Allow and process URIs pasted to the payTo field of SendCoinsEntry (#3475)
    * qt: Allow and process URIs pasted to the payTo field of SendCoinsEntry
    
    * qt: Prevent double invocation of the textChanged slot when pasting an URI
    
    * qt: Keep the addressbook's label if there is already one for the address
    
    If there is no label for the given address in the addressbook but the 
    URI contains a label it will use the URI's label to update the 
    addressbook.
    
    * qt: Always prefer the addressbook's label over an URI's label

--- src/qt/sendcoinsentry.cpp
+++ src/qt/sendcoinsentry.cpp
@@ -79,7 +79,14 @@ void SendCoinsEntry::on_addressBookButton_clicked()
 
 void SendCoinsEntry::on_payTo_textChanged(const QString &address)
 {
-    updateLabel(address);
+    SendCoinsRecipient rcp;
+    if (GUIUtil::parseBitcoinURI(address, &rcp)) {
+        ui->payTo->blockSignals(true);
+        setValue(rcp);
+        ui->payTo->blockSignals(false);
+    } else {
+        updateLabel(rcp.address);
+    }
 }
 
 void SendCoinsEntry::setModel(WalletModel *_model)
@@ -217,12 +224,12 @@ void SendCoinsEntry::setValue(const SendCoinsRecipient &value)
         ui->messageTextLabel->setVisible(!recipient.message.isEmpty());
         ui->messageLabel->setVisible(!recipient.message.isEmpty());
 
-        ui->addAsLabel->clear();
-        ui->payTo->setText(recipient.address); // this may set a label from addressbook
-        if (!recipient.label.isEmpty()) // if a label had been set from the addressbook, don't overwrite with an empty label
-            ui->addAsLabel->setText(recipient.label);
+        ui->payTo->setText(recipient.address);
+        ui->addAsLabel->setText(recipient.label);
         ui->payAmount->setValue(recipient.amount);
     }
+
+    updateLabel(recipient.address);
 }
 
 void SendCoinsEntry::setAddress(const QString &address)
