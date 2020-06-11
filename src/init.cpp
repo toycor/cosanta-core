86a4fe4d64a32c3d9b25746c1308c4b217e384a6
UdjinM6 <UdjinM6@users.noreply.github.com>
2020-06-11 20:52:58 +0300

    UpgradeDBIfNeeded failure should require reindexing (#3516)

--- src/init.cpp
+++ src/init.cpp
@@ -1930,7 +1930,10 @@ bool AppInitMain()
                     assert(chainActive.Tip() != NULL);
                 }
 
-                deterministicMNManager->UpgradeDBIfNeeded();
+                if (!deterministicMNManager->UpgradeDBIfNeeded()) {
+                    strLoadError = _("Error upgrading evo database");
+                    break;
+                }
 
                 if (!is_coinsview_empty) {
                     uiInterface.InitMessage(_("Verifying blocks..."));
