b65364771d78f083f806e287c9249fc9f75b3248
dustinface <35775977+xdustinface@users.noreply.github.com>
2020-12-11 00:10:40 +0100

    privatesend: Drop redundant CBLSSignature.IsValid() checks (#3866)
    It's checked in VerifyInsecure already

--- src/privatesend/privatesend.cpp
+++ src/privatesend/privatesend.cpp
@@ -64,7 +64,7 @@ bool CPrivateSendQueue::CheckSignature(const CBLSPublicKey& blsPubKey) const
 
     CBLSSignature sig;
     sig.SetBuf(vchSig);
-    if (!sig.IsValid() || !sig.VerifyInsecure(blsPubKey, hash)) {
+    if (!sig.VerifyInsecure(blsPubKey, hash)) {
         LogPrint(BCLog::PRIVATESEND, "CPrivateSendQueue::CheckSignature -- VerifyInsecure() failed\n");
         return false;
     }
@@ -114,7 +114,7 @@ bool CPrivateSendBroadcastTx::CheckSignature(const CBLSPublicKey& blsPubKey) con
 
     CBLSSignature sig;
     sig.SetBuf(vchSig);
-    if (!sig.IsValid() || !sig.VerifyInsecure(blsPubKey, hash)) {
+    if (!sig.VerifyInsecure(blsPubKey, hash)) {
         LogPrint(BCLog::PRIVATESEND, "CPrivateSendBroadcastTx::CheckSignature -- VerifyInsecure() failed\n");
         return false;
     }
