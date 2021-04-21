e26b4dd18dbb74534cc02fd4975d68ec910b4ac7
Dmitriy Korniychuk <dmitriy@kornychuk.org.ua>
2021-04-21 20:28:49 +0300

    Fix icon location

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -468,10 +468,10 @@ void BitcoinGUI::createActions()
     showBackupsAction = new QAction(QIcon(":/icons/browse"), tr("Show Automatic &Backups"), this);
     showBackupsAction->setStatusTip(tr("Show automatically created wallet backups"));
     // POW Actions
-    powStartAction = new QAction(QIcon(":/icons//icons/pow_active"), tr("Start PoW mining"), this);
+    powStartAction = new QAction(QIcon(":/icons/pow_active"), tr("Start PoW mining"), this);
     powStartAction->setStatusTip(tr("Start mining Cosanta"));
     powStartAction->setVisible(true);
-    powStopAction = new QAction(QIcon(":/icons//icons/pow_deactive"), tr("Stop PoW mining"), this);
+    powStopAction = new QAction(QIcon(":/icons/pow_deactive"), tr("Stop PoW mining"), this);
     powStopAction->setStatusTip(tr("Stop mining Cosanta"));
     powStopAction->setVisible(false);
     // initially disable the debug window menu items
