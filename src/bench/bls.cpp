bytzck/BYTZ

Merge #11517: Tests: Improve benchmark precision (#3620)
* Merge #11517: Tests: Improve benchmark precision

760af84 Removed CCheckQueueSpeed benchmark (Martin Ankerl)
00721e6 Improved microbenchmarking with multiple features. (Martin Ankerl)

Pull request description:

  The benchmark's KeepRunning() used to make a function call for each call, inflating measurement times for short running code. This change inlines the critical code that is executed each run and moves the slow timer updates into a new function.

  This change increases the average runtime for Trig from 0.000000082339208 sec to 0.000000080948591.

Tree-SHA512: 36b3bc55fc9b1d4cbf526b7103af6af18e9783e6b8f3ad3adbd09fac0bf9401cfefad58fd1e6fa2615d3c4e677998f912f3323d61d7b00b1c660d581c257d577
Signed-off-by: pasta <pasta@dashboost.org>

# Conflicts:
#	src/bench/bench.cpp
#	src/bench/bench_dash.cpp
#	src/bench/crypto_hash.cpp
#	src/bench/prevector_destructor.cpp
#	src/bench/verify_script.cpp

* More of 11517

Co-authored-by: Wladimir J. van der Laan <laanwj@gmail.com>
Co-authored-by: UdjinM6 <UdjinM6@users.noreply.github.com>

src/bench/bls.cpp
@@ -348,15 +348,15 @@ static void BLSVerify_BatchedParallel(benchmark::State& state)
     }
 }
 
-BENCHMARK(BLSPubKeyAggregate_Normal)
-BENCHMARK(BLSSecKeyAggregate_Normal)
-BENCHMARK(BLSSign_Normal)
-BENCHMARK(BLSVerify_Normal)
-BENCHMARK(BLSVerify_LargeBlock1000)
-BENCHMARK(BLSVerify_LargeBlockSelfAggregated1000)
-BENCHMARK(BLSVerify_LargeBlockSelfAggregated10000)
-BENCHMARK(BLSVerify_LargeAggregatedBlock1000)
-BENCHMARK(BLSVerify_LargeAggregatedBlock10000)
-BENCHMARK(BLSVerify_LargeAggregatedBlock1000PreVerified)
-BENCHMARK(BLSVerify_Batched)
-BENCHMARK(BLSVerify_BatchedParallel)
+BENCHMARK(BLSPubKeyAggregate_Normal, 300 * 1000)
+BENCHMARK(BLSSecKeyAggregate_Normal, 700 * 1000)
+BENCHMARK(BLSSign_Normal, 600)
+BENCHMARK(BLSVerify_Normal, 350)
+BENCHMARK(BLSVerify_LargeBlock1000, 1)
+BENCHMARK(BLSVerify_LargeBlockSelfAggregated1000, 1)
+BENCHMARK(BLSVerify_LargeBlockSelfAggregated10000, 1)
+BENCHMARK(BLSVerify_LargeAggregatedBlock1000, 1)
+BENCHMARK(BLSVerify_LargeAggregatedBlock10000, 1)
+BENCHMARK(BLSVerify_LargeAggregatedBlock1000PreVerified, 5)
+BENCHMARK(BLSVerify_Batched, 500)
+BENCHMARK(BLSVerify_BatchedParallel, 1000)

toycor/cosanta-core

src/bench/bls.cpp
@@ -349,15 +349,15 @@ static void BLS_Verify_BatchedParallel(benchmark::State& state)
     }
 }
 
-BENCHMARK(BLS_PubKeyAggregate_Normal)
-BENCHMARK(BLS_SecKeyAggregate_Normal)
-BENCHMARK(BLS_Sign_Normal)
-BENCHMARK(BLS_Verify_Normal)
-BENCHMARK(BLS_Verify_LargeBlock1000)
-BENCHMARK(BLS_Verify_LargeBlockSelfAggregated1000)
-BENCHMARK(BLS_Verify_LargeBlockSelfAggregated10000)
-BENCHMARK(BLS_Verify_LargeAggregatedBlock1000)
-BENCHMARK(BLS_Verify_LargeAggregatedBlock10000)
-BENCHMARK(BLS_Verify_LargeAggregatedBlock1000PreVerified)
-BENCHMARK(BLS_Verify_Batched)
-BENCHMARK(BLS_Verify_BatchedParallel)
+BENCHMARK(BLS_PubKeyAggregate_Normal, 300 * 1000)
+BENCHMARK(BLS_SecKeyAggregate_Normal, 700 * 1000)
+BENCHMARK(BLS_Sign_Normal, 600)
+BENCHMARK(BLS_Verify_Normal, 350)
+BENCHMARK(BLS_Verify_LargeBlock1000, 1)
+BENCHMARK(BLS_Verify_LargeBlockSelfAggregated1000, 1)
+BENCHMARK(BLS_Verify_LargeBlockSelfAggregated10000, 1)
+BENCHMARK(BLS_Verify_LargeAggregatedBlock1000, 1)
+BENCHMARK(BLS_Verify_LargeAggregatedBlock10000, 1)
+BENCHMARK(BLS_Verify_LargeAggregatedBlock1000PreVerified, 5)
+BENCHMARK(BLS_Verify_Batched, 500)
+BENCHMARK(BLS_Verify_BatchedParallel, 1000)