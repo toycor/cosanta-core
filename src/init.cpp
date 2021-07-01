f7c1c9992084e41ee5ed74979a38ea9409923fc2
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2021-07-01 11:38:34 +0300

    DEFAULT_DISABLE_SAFEMODE was moved to safemode.h

--- src/init.cpp
+++ src/init.cpp
@@ -98,7 +98,6 @@
 bool fFeeEstimatesInitialized = false;
 static const bool DEFAULT_PROXYRANDOMIZE = true;
 static const bool DEFAULT_REST_ENABLE = false;
-static const bool DEFAULT_DISABLE_SAFEMODE = false;
 static const bool DEFAULT_STOPAFTERBLOCKIMPORT = false;
 
 
